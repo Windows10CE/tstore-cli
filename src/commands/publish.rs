9fd6b66d7fab42f318e9b25b6b54435a4fe71961
Aaron Robinson <aaronrobin1234@gmail.com>
2021-04-11 20:20:45 -0500

    Finish config file support for publish command, add error messages

--- src/commands/publish.rs
+++ src/commands/publish.rs
@@ -1,3 +1,4 @@
+use crate::models::package_info::PackageInfo;
 use clap::{App, Arg, ArgMatches, SubCommand};
 use reqwest::{
     blocking::{multipart, Client},
@@ -11,7 +12,7 @@ pub fn create_subcommand() -> App<'static, 'static> {
         .about("Publish a ZIP to thunderstore")
         .arg(
             Arg::with_name("zip")
-                .help("ZIP file to publish")
+                .help("ZIP file to publish\nCheck the documentation at https://github.com/Windows10CE/tstore-cli for additional notes on usage.")
                 .takes_value(true)
                 .validator(file_exists),
         )
@@ -41,11 +42,17 @@ pub fn create_subcommand() -> App<'static, 'static> {
                 .help("Package contains nsfw contents")
                 .long("nsfw"),
         )
+        .arg(
+            Arg::with_name("config")
+                .help("The config file to read from.")
+                .long("config")
+                .default_value("publish.toml")
+        )
 }
 
 fn file_exists(filename: String) -> Result<(), String> {
     if std::fs::File::open(filename).is_err() {
-        return Err(String::from("ZIP file doesn't exist"));
+        return Err("ZIP file doesn't exist".to_string());
     }
     Ok(())
 }
@@ -54,10 +61,8 @@ pub fn process_command(
     matches: &ArgMatches,
     url: String,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let options = PublishOptions::from_matches_and_file(
-        matches,
-        matches.value_of("config").unwrap_or("publish.toml"),
-    )?;
+    let options =
+        PublishOptions::from_matches_and_file(matches, matches.value_of("config").unwrap())?;
 
     let metadata_json = json!({
         "author_name": options.author_name,
@@ -72,21 +77,25 @@ pub fn process_command(
         .file("file", options.zip)
         .unwrap();
 
-    let res = Client::new()
+    let req = Client::new()
         .post(format!("{}/experimental/package/upload/", url))
         .bearer_auth(options.token)
-        .multipart(form)
-        .send()?;
+        .multipart(form);
+
+    println!("Uploading package...");
+    let res = req.send()?;
 
     if res.status() != StatusCode::OK {
         return Err(From::from(res.text()?));
     }
 
-    println!("Package uploaded successfully!");
-    println!(
-        "{}",
-        serde_json::to_string_pretty(&serde_json::Value::from(res.text()?))?
-    );
+    let res_json = res.text()?.parse::<serde_json::Value>()?;
+    let new_info = PackageInfo::from_author_and_name(
+        res_json["namespace"].as_str().unwrap(),
+        res_json["name"].as_str().unwrap(),
+        url.as_str(),
+    )?;
+    println!("Package uploaded successfully! {}", new_info.package_url);
 
     Ok(())
 }
@@ -108,62 +117,65 @@ impl PublishOptions {
         if let Ok(backup) = std::fs::read_to_string(backup_file_path) {
             let backup_options = backup.parse::<Value>()?;
             return Ok(PublishOptions {
-                author_name: String::from(
-                    matches
-                        .value_of("author")
-                        .or_else(|| backup_options["team_name"].as_str())
-                        .ok_or("")?,
-                ),
+                author_name: matches
+                    .value_of("author")
+                    .or_else(|| backup_options.get("author").map(|x| x.as_str().unwrap()))
+                    .ok_or("This command requires an author name to upload under. Check help using tstore-cli publish --help")?
+                    .to_string(),
                 categories: matches
                     .values_of_lossy("categories")
                     .or_else(|| {
-                        backup_options["categories"].as_array().map(|x| {
-                            x.iter()
-                                .map(|y| String::from(y.as_str().unwrap()))
-                                .collect()
-                        })
+                        if let Some(backup_option) = backup_options.get("categories") {
+                            return backup_option.as_array().map(|x| {
+                                x.iter()
+                                    .map(|y| y.as_str().unwrap().to_string())
+                                    .collect()
+                            });
+                        }
+                        None
                     })
                     .unwrap_or(vec![]),
                 communities: matches
                     .values_of_lossy("communities")
                     .or_else(|| {
-                        backup_options["communities"].as_array().map(|x| {
-                            x.iter()
-                                .map(|y| String::from(y.as_str().unwrap()))
-                                .collect()
-                        })
+                        if let Some(backup_option) = backup_options.get("communities") {
+                            return backup_option.as_array().map(|x| {
+                                x.iter()
+                                    .map(|y| y.as_str().unwrap().to_string())
+                                    .collect()
+                            });
+                        }
+                        None
                     })
-                    .ok_or("")?,
+                    .ok_or("This command requires you to specify one or more communities to upload to. Check help using tstore-cli publish --help")?,
                 nsfw: matches.is_present("nsfw")
-                    || backup_options["nsfw"].as_bool().unwrap_or(false),
-                zip: String::from(
-                    matches
-                        .value_of("zip")
-                        .or_else(|| {
-                            if let Some(other_zip) = backup_options["zip"].as_str() {
-                                if file_exists(String::from(other_zip)).is_ok() {
-                                    return Some(other_zip);
-                                }
+                    || backup_options.get("nsfw").map(|x| x.as_bool().unwrap()).unwrap_or(false),
+                zip: matches
+                    .value_of("zip")
+                    .or_else(|| {
+                        if let Some(other_zip) = backup_options.get("zip").map(|x| x.as_str().unwrap()) {
+                            if file_exists(other_zip.to_string()).is_ok() {
+                                return Some(other_zip);
                             }
-                            None
-                        })
-                        .ok_or("")?,
-                ),
-                token: String::from(
-                    matches
-                        .value_of("token")
-                        .or_else(|| backup_options["token"].as_str())
-                        .ok_or("This command requires a service account token! Use -t, --token, the the TSTORE_TOKEN env var to set the token.")?,
-                ),
+                        }
+                        None
+                    })
+                    .ok_or("This command requires a path to a ZIP file to upload. Check help using tstore-cli publish --help")?
+                    .to_string(),
+                token: matches
+                    .value_of("token")
+                    .or_else(|| backup_options.get("token").map(|x| x.as_str().unwrap()))
+                    .ok_or("This command requires a service account token! Check help using tstore-cli publish --help")?
+                    .to_string(),
             });
         } else {
             return Ok(PublishOptions {
-                author_name: String::from(matches.value_of("author").ok_or("")?),
+                author_name: matches.value_of("author").ok_or("This command requires an author name to upload under. Check help using tstore-cli publish --help")?.to_string(),
                 categories: matches.values_of_lossy("categories").unwrap_or(vec![]),
-                communities: matches.values_of_lossy("").ok_or("")?,
+                communities: matches.values_of_lossy("communities").ok_or("This command requires you to specify one or more communities to upload to. Check help using tstore-cli publish --help")?,
                 nsfw: matches.is_present("nsfw"),
-                zip: String::from(matches.value_of("zip").ok_or("")?),
-                token: String::from(matches.value_of("token").ok_or("This command requires a service account token! Use -t, --token, the the TSTORE_TOKEN env var to set the token.")?),
+                zip: matches.value_of("zip").ok_or("This command requires a path to a ZIP file to upload. Check help using tstore-cli publish --help")?.to_string(),
+                token: matches.value_of("token").ok_or("This command requires a service account token! Check help using tstore-cli publish --help")?.to_string(),
             });
         }
     }
